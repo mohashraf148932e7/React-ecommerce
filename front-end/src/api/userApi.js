a2bf39bb21b29193b69d111a1bb31c7cf76aec0a
mohashraf148932e7 <ashraf.m.1414@gmail.com>
2025-02-24 04:08:00 +0200

    product-update

---
+++ front-end/src/api/userApi.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3000/users";
+
+const getAllUsers = async () => axios.get(baseUrl);
+const getUserById = async (id) => axios.get(`${baseUrl}/${id}`);
+const addUser = async (user) => axios.post(`${baseUrl}`, user);
+const updateUser = async (id, user) => axios.put(`${baseUrl}/${id}`, user);
+const deleteUser = async (id) => axios.delete(`${baseUrl}/${id}`);
+
+export { getAllUsers, getUserById, addUser, updateUser, deleteUser };
